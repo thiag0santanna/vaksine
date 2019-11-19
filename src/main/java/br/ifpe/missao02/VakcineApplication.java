6bef85f29610145d4500429d7cc654d3e1770eb5
aluno <aluno@ln179860>
2019-11-19 20:32:17 -0300

    primeiro commit

---
+++ src/main/java/br/ifpe/missao02/VakcineApplication.java
@@ -0,0 +1,13 @@
+package br.ifpe.missao02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VakcineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VakcineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ifpe/missao02/VakcineApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ifpe.missao02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VakcineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
