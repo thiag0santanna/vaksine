232458595e5e871bf97e88f39de7387f2d557fe2
gabriel lima <gabriel.luiz.g.lima@accenture.com>
2019-11-28 16:43:49 -0300

    sprint 1 cadastro agente backend

--- src/main/java/com/projeto/controller/app/AgenteController.java
+++ src/main/java/com/projeto/controller/app/AgenteController.java
@@ -1,5 +1,29 @@
 package com.projeto.controller.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.projeto.dao.app.AgenteDAO;
+import com.projeto.model.app.Agente;
+
+@Controller
 public class AgenteController {
+	
+	@Autowired
+	private AgenteDAO agenteDAO;
+	
+	@RequestMapping(value = "/formagente", method = RequestMethod.GET)
+	public String exibirCadastroAgente() {
+		return "cadastro-agente";
+	}
+	
+	@RequestMapping(value = "cadastroagente", method = RequestMethod.POST)
+	public String cadastrarAgente(Agente agente) {
+		agenteDAO.save(agente);
+		return "cadastroagente";
+	}
+	
 
 }
--- src/main/java/com/projeto/model/app/Agente.java
+++ src/main/java/com/projeto/model/app/Agente.java
@@ -1,5 +1,72 @@
 package com.projeto.model.app;
 
-public class Agente {
+import java.util.Calendar;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+public class Agente {
+	
+	@Id
+	@GeneratedValue
+	private Integer matricula;
+	
+	@NotBlank(message="O campo Nome precisa ser preenchido")
+	private String nome;
+	
+	@NotBlank(message="O campo CPF precisa ser preenchido")
+	private String cpf;
+	
+	@NotBlank(message="O campo Rg precisa ser preenchido")
+	private String rg;
+	
+	@NotBlank(message="O campo Email precisa ser preenchido")
+	private String email;
+	
+	@NotNull(message="O campo Data de nascimento precisa ser preenchido")
+	@DateTimeFormat(pattern="dd/MM/yyyy")
+	private Calendar dataNascimento;
+	
+	public Integer getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(Integer matricula) {
+		this.matricula = matricula;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getRg() {
+		return rg;
+	}
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Calendar getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(Calendar dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
 }
